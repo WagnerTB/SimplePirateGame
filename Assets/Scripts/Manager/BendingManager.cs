508ef6aa31589d2f73e2bf5bae6f0e070fbcbe68
WagnerTB <wagner_madara@hotmail.com>
2020-09-22 19:23:53 -0300

    Added World bending shader

---
+++ Assets/Scripts/Manager/BendingManager.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+[ExecuteAlways]
+public class BendingManager : MonoBehaviour
+{
+    private string keyword = "ENABLE_BENDING";
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (Application.isPlaying)
+            Shader.EnableKeyword(keyword);
+        else
+            Shader.DisableKeyword(keyword);
+    }
+
+    private void OnEnable()
+    {
+        RenderPipelineManager.beginCameraRendering += OnBeginCameraRendering;
+        RenderPipelineManager.endCameraRendering += OnEndCameraRendering;
+    }
+
+    private void OnDestroy()
+    {
+        RenderPipelineManager.beginCameraRendering -= OnBeginCameraRendering;
+        RenderPipelineManager.endCameraRendering   -= OnEndCameraRendering;
+    }
+
+    private void OnBeginCameraRendering(ScriptableRenderContext ctx, Camera cam)
+    {
+        cam.cullingMatrix = Matrix4x4.Ortho(-99, 99, -99, 99, 0.001f, 99) * cam.worldToCameraMatrix;
+    }
+
+    private void OnEndCameraRendering(ScriptableRenderContext ctx, Camera cam)
+    {
+        cam.ResetCullingMatrix();
+    }
+
+    
+
+    [ContextMenu("Disable")]
+    public void Disable()
+    {
+        Shader.DisableKeyword(keyword);
+
+    }
+
+    [ContextMenu("Enable")]
+    public void Enable()
+    {
+        Shader.EnableKeyword(keyword);
+
+    }
+}

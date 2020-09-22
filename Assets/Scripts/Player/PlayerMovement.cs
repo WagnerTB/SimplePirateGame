f90e10d44c0e23b8e94ac81bf74df5fcdd2bb342
WagnerTB <wagner_madara@hotmail.com>
2020-09-22 12:57:20 -0300

    Finished basic player movement

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -2,8 +2,14 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+[RequireComponent(typeof(Rigidbody2D))]
 public class PlayerMovement : MonoBehaviour
 {
+    public float speed = 30;
+    public float maxSpeed = 30;
+    public float rotateSpeed = 100;
+    public Rigidbody2D rb;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +20,43 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         
+    }
+
+    private void FixedUpdate()
+    {
+        var Horizontal = Input.GetAxis("Horizontal");
+        var Vertical = Input.GetAxis("Vertical");
+
+        Move(Vertical);
+        Rotate(Horizontal);
+    }
+
+    private void Move(float input)
+    {
+        var direction = transform.right * input * speed * Time.deltaTime;
+        // rb.MovePosition(transform.position + direction);
+        if(rb.velocity.magnitude < maxSpeed)
+            rb.AddForce(direction); 
+    }
+
+    private void Rotate(float input)
+    {
+        Vector3 rotate = new Vector3(0, 0 , -input * rotateSpeed);
+
+        Quaternion deltaRotation = Quaternion.Euler(rotate * Time.deltaTime);
+        rb.MoveRotation(transform.rotation * deltaRotation);
+    }
+
+
+
+
+    private void Reset()
+    {
+        var rb = GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            this.rb = rb;
+            rb.gravityScale = 0;
+        }
     }
 }
